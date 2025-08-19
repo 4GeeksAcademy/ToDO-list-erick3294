3a99ab8194711f360ac7b14e0542b0eb18f3f878
Erick Sanchez <erick94310@gmail.com>
2025-08-19 03:01:44 +0000

    TO-DO-LISTS

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,27 +1,77 @@
-import React from "react";
+import React, { useState } from "react";
+import "./Lists.css"
+
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
+import { renderIntoDocument } from "react-dom/test-utils";
 
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-            
+	const [newChore, setNewChores] = useState('');
+	const [myTasks, setMyTasks] = useState(["Clean Room", "Clean Kitchen", "Wash Car", "Make Bed", "Dust"])
+	const nextChore = () => {
+		setMyTasks([newChore, ...myTasks])
+		setNewChores("")
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+	}
+	const deleteTask = (item) => {
+		const filteredTasks = myTasks.filter(
+			(choreData) => choreData != item
+		)
+		console.log("dust: ", filteredTasks)
+		setMyTasks(filteredTasks)
+	}
+	return (
+		<div className="text-center container">
+			<h1>To Do List</h1>
+			<div className="toDo container-fluid">
+			<input
+			value ={newChore}
+				className="add"
+				type="text"
+				placeholder="Add New task"
+				
+				onChange={(e) => {
+					const newTask = e.target.value
+					setNewChores(newTask)
+				}
+				}
+				onKeyDown={
+					(e) => {
+						if (e.key == "Enter") {
+							nextChore(newChore)
+						}
+					}
+				}
+
+			/>
+			
+			<button onClick={() => nextChore()}>
+				Add chore
+			</button>
+			<ul>
+				<li className="things">{myTasks.map(
+					(item, index) => {
+						return (
+							<div>
+								<li key={index + "chore"}>{item}
+									<span className=" text-danger " 
+									onClick={() => {
+									deleteTask(item)
+								}
+								}
+								>X</span>
+								</li>
+								
+							</div>
+						)
+					}
+				)}
+				</li>
+			</ul>
+			</div>
+		</div >
 	);
 };
 
