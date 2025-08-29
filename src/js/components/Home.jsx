e28d8b32fc0d5176d10d453ee8cbeed3dcccfd97
Erick Sanchez <erick94310@gmail.com>
2025-08-29 22:07:35 +0000

    fetch

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -20,63 +20,91 @@ const createUser = () => {
 	fetch("https://playground.4geeks.com/todo/users/erick3294", options)
 		.then((r) => r.json())
 		.then((d) => console.log("creater-user-data", d))
+}
+const updatingToDo = () => {
+	const options = {
+		method: "PUT",
+		headers: { "content-type": "application/json" },
+		body: JSON.stringify({
+			"label": "string",
+			"is_done": true
+		})
+	}
 
+	fetch(url + "/todos/37", options)
+		.then((resp) => resp.json())
+		.then((data) => console.log("updated toDOs:", data))
 }
 const getAllUsers = () => {
-	fetch(url + "/users")
+	fetch(url + "/users/erick3294")
 		.then(
 			(resp) => {
-				console.log("response:", resp)
+				console.log("getAllUsersResponse", resp)
 				return resp.json()
 			}
 		)
 		.then(
-			(data) => (console.log("data:", data))
+			(data) => (console.log("get all users data", data))
 		)
 }
-const addToDo = ()=>{
-	let options = {
-		method:"POST",
-		headers:{"content-type":"application/json"},
-		body: JSON.stringify({
-			"label": "clean the pool",
-			"is_done":false
-			
-
-		})
-	}
-	fetch(url+"/todos/erick3294", options)
-	.then((resp)=>resp.json()) 
-	.then((data)=> console.log("addToDo", data))
-}
 
 
-
-const getToDos = () => {
-	fetch( url + "/users/erick3294")
-		.then((resp) => { return resp.json() })
-		.then((data) => { console.log("dataUsers:", data) })
-}
-
-
-//create your first component
 const Home = () => {
-	useEffect(
-		() => {
-			addToDo()
-			getToDos()
-			createUser()
-		}, []
-	)
-
 
 	const [newChore, setNewChores] = useState('');
 	const [myTasks, setMyTasks] = useState(["Clean Room", "Clean Kitchen", "Wash Car", "Make Bed", "Dust"])
-	const nextChore = () => {
+	const updateChoresArray = () => {
 		setMyTasks([newChore, ...myTasks])
 		setNewChores("")
 
+	};
+
+const getToDos = () => {
+	console.log("getToDOs")
+	fetch(url + "/users/erick3294")
+		.then((resp) => { return resp.json() })
+		.then((data) => { 
+			setMyTasks(data.todos)
+			console.log("dataUsers:", data.todos) })
+}
+
+const addToDo = (Label) => {
+	let options = {
+		method: "POST",
+		headers: { "content-type": "application/json" },
+		body: JSON.stringify({
+			"label": Label.label,
+			"is_done": false
+		})
 	}
+	fetch(url + "/todos/erick3294", options)
+		.then((resp) => resp.json())
+		.then((data) => {console.log("addToDo", data)
+			getToDos()
+		})
+}
+const deleteToDo = (ID) => {
+	const options = {
+		method: "DELETE",
+		headers: { "content-type": "application/json" },
+
+	}
+	fetch(url + "/todos/" + ID, options)
+		.then((resp) => resp.json())
+		.then((data) => {console.log("deleteToDo:", data)
+			getToDos()
+		})
+}
+
+	useEffect(
+		() => {
+			createUser()
+			getToDos()
+			
+		}, []
+	);
+	console.log("chores tag:", myTasks)
+
 	const deleteTask = (item) => {
 		const filteredTasks = myTasks.filter(
 			(choreData) => choreData != item
@@ -89,39 +117,46 @@ const Home = () => {
 			<h1>To Do List</h1>
 			<div className="toDo container-fluid">
 				<input
-					value={newChore}
+					value={newChore.label}
 					className="add"
 					type="text"
-					placeholder="Add New task"
-
+					placeholder="Add New task"					
 					onChange={(e) => {
 						const newTask = e.target.value
-						setNewChores(newTask)
+						setNewChores({label:newTask, is_done: false})
 					}
 					}
 					onKeyDown={
 						(e) => {
 							if (e.key == "Enter") {
-								nextChore(newChore)
+								updateChoresArray(newChore)
+								addToDo(newChore)
 							}
 						}
 					}
 
 				/>
 
-				<button onClick={() => nextChore()}>
+				<button onClick={() => {
+					updateChoresArray(newChore)
+								addToDo(newChore)
+				}}>
 					Add chore
 				</button>
-				<button onClick={()=> addToDo()}>Add to API</button>
+				{/* <button onClick={() => addToDo()}>Add to API</button>
+				<button onClick={() => updatingToDo()}>update</button>
+				<button onClick={() => deleteToDo()}>delete</button> */}
 				<ul>
-					<li className="things">{myTasks.map(
+					<li className="things">
+					{myTasks.map(
 						(item, index) => {
+							console.log("mapItem", item.id)
 							return (
 								<div>
-									<li key={index + "chore"}>{item}
+									<li key={index + "chore"}>{item.label}
 										<span className=" text-danger "
 											onClick={() => {
-												deleteTask(item)
+												deleteToDo(item.id)
 											}
 											}
 										>X</span>
